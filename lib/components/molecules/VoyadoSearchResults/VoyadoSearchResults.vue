cb1be0323d376e2c2ceb69255f6477ff01108fbe
Kevin Fitzpatrick <fitzpatrick@carismar.com>
2023-04-03 10:50:02 +0200

    fix: typo

--- lib/components/molecules/VoyadoSearchResults/VoyadoSearchResults.vue
+++ lib/components/molecules/VoyadoSearchResults/VoyadoSearchResults.vue
@@ -30,7 +30,7 @@
           </CaIconAndText>
         </button>
       </div>
-      <VoyadoSearchProductReults :products="products" />
+      <VoyadoSearchProductResults :products="products" />
     </div>
     <div
       v-else-if="!hasProducts && !isLoading"
