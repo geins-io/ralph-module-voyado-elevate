668027599f7ba4f85911f5c05310bd977588e800
Olivia Axelsson <axelsson@carismar.com>
2023-04-21 16:17:09 +0100

    fix: change to mixin

--- lib/components/molecules/VoyadoSearchForm/VoyadoSearchForm.vue
+++ lib/components/molecules/VoyadoSearchForm/VoyadoSearchForm.vue
@@ -38,7 +38,7 @@
 </template>
 <script>
 import { debounce } from 'lodash';
-import VoyadoProps from '@geins/ralph-module-voyado-elevate/lib/components/mixins/VoyadoProps.mjs';
+import VoyadoProps from '@geins/ralph-module-voyado-elevate/lib/components/mixins/VoyadoProps.js';
 
 export default {
   name: 'VoyadoSearchForm',
